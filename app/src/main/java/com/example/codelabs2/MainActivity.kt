f9744096e144f627061f2b23835b3617eb84536e
boris.loor06 <boris.loor06@gmail.com>
2022-10-08 04:51:17 -0500

    Cuarta práctica Kotlin

--- app/src/main/java/com/example/codelabs2/MainActivity.kt
+++ app/src/main/java/com/example/codelabs2/MainActivity.kt
@@ -3,8 +3,8 @@ package com.example.codelabs2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +17,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
+        // Crear un Objeto Dado con 6 caras y tirarlo, para determinar que sale
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+
+        // Seleccionar la imagen de la vista
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        //Determinar cual id del recurso usar en base al tiro del dado
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Actualiar la vista de imagen con el correcto id del recurso
+        diceImage.setImageResource(drawableResource)
+
+        // Actualizar el contenido de la descripción
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
